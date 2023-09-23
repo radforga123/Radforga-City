82a11b96dda9708662fd15b60a6fa2a16d3cf5c3
Radforga <radmir2301@gmail.com>
2023-09-23 21:17:49 +0300

    Comments

---
+++ build/CMakeFiles/BattleCity.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BattleCity.exe"
+  "BattleCity.exe.manifest"
+  "BattleCity.pdb"
+  "CMakeFiles/BattleCity.dir/src/main.cpp.obj"
+  "CMakeFiles/BattleCity.dir/src/main.cpp.obj.d"
+  "libBattleCity.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BattleCity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/BattleCity.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BattleCity.
+# This may be replaced when dependencies are built.
